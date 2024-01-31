7b8905b4176f5483f7a40758087ef054fbce4478
domic1 <115622468+domic1@users.noreply.github.com>
2024-01-31 19:57:23 +0100

    Dodaj pliki projektów.

---
+++ Works with numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Done_is_better_than_good
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("Podaj pierwszą cyfrę");
+            number1 =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Podaj drugą cyfrę");
+            number2 =Convert.ToInt32( Console.ReadLine());
+
+            Console.WriteLine("Wynik:" + (number1 + number2));
+
+        }
+    }
+}
